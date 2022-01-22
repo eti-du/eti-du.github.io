8c525bfba1cffbf07a5a982639ea5ea7a6c9c992
eti-du <92732299+eti-du@users.noreply.github.com>
2022-01-22 15:50:58 +0100

    Update script.js

--- script.js
+++ script.js
@@ -6,3 +6,17 @@ function beep() {
 function cursor() {
     document.getElementById('pageetbarre').style.cursor = 'none';
 }
+
+function quiz() {
+    document.getElementById("submitbutton").style.display = 'none';
+    var points = 0;
+    var a = document.querySelector('input[name="ques1"]:checked').value;
+    var b = document.querySelector('input[name="ques2"]:checked').value;
+    var c = document.querySelector('input[name="ques3"]:checked').value;
+    var d = document.querySelector('input[name="ques4"]:checked').value;
+    if (a === 'prop2') {points++;}
+    if (b === 'prop1') {points++;}
+    if (c === 'prop2') {points++;}
+    if (d === 'prop3') {points++;}
+    document.getElementById("points").innerHTML = points + "/4";
+}
